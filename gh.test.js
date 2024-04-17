dd8a5e657b5293af4d88800efb3ce4ffc15ef476
KleoVor <t9671253993@gmail.com>
2024-04-17 16:33:45 +0300

    beforeAll и afterAll вынесены за describe

--- gh.test.js
+++ gh.test.js
@@ -1,86 +1,72 @@
 let page;
 
+beforeAll(async () => {
+  page = await browser.newPage();
+});
+
+afterAll(async () => {
+  await page.close();
+});
+
 describe("Github page new tests", () => {
  
  beforeEach(async () => {
-  page = await browser.newPage();
   await page.goto("https://github.com/account/organizations/new?plan=free&ref_cta=Create%2520a%2520new%2520organization&ref_loc=bottom%2520plans&ref_page=%2Fteam");
 });
  
-afterEach(async () => {
-  page.close();
-});
-
-test("The first link attribute", async () => {
-   
+test("The first link attribute", async () => {   
   await page.waitForSelector('a', {timeout: 2000});
-  const actual = await page.$eval("a", link => link.getAttribute('href') );
+  const actual = await page.$eval("a", link => link.getAttribute('href'));
   expect(actual).toEqual("#start-of-content");
 });
-
 
 test("The login Field is the active element", async () => {
   const isCursorInsideField = await page.evaluate(() => {
     const loginField = document.querySelector('#login_field');
-    return loginField === document.activeElement; // Проверяем, является ли login_field активным элементом
+    return loginField === document.activeElement;
   });
   
   console.log('Курсор установлен в login_field:', isCursorInsideField);
 });
 
-
-
 test("Check if signInButton is clickable", async () => {
   const signInButtonSelector = "#login > div.auth-form-body.mt-3 > form > div > input.btn.btn-primary.btn-block.js-sign-in-button";
   
-  // Ждем, пока кнопка станет видимой
   await page.waitForSelector(signInButtonSelector, { visible: true, timeout: 2000 });
 
-  // Проверяем кликабельность кнопки
   const isClickable = await page.$eval(signInButtonSelector, button => !button.disabled && button.offsetParent !== null);
 
-  // Проверяем, является ли кнопка кликабельной (ожидаемое - кнопка кликабельна)
   expect(isClickable).toBe(true);
 });
 
 });
 
-
-
 describe("Github page tests", () => {
-  let page;
 
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("https://github.com/team");
-  });
+ beforeEach(async () => {
+  await page.goto("https://github.com/team");
+ });
 
-
-  afterEach(() => {
-    page.close();
-  });
-
-  test("The h1 header content'", async () => {
+test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1', { timeout: 2000 });
-    const title2 = await page.title();
-    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
-  });
+    const title = await page.title();
+    expect(title).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+});
 
-  test("The first link attribute", async () => {
-   
-    await page.waitForSelector('a', {timeout: 2000});
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
-    expect(actual).toEqual("#start-of-content");
-  });
+test("The first link attribute", async () => {   
+  await page.waitForSelector('a', {timeout: 2000});
+  const actual = await page.$eval("a", link => link.getAttribute('href') );
+  expect(actual).toEqual("#start-of-content");
+});
 
-  test("The page contains Sign in button", async () => {
-    const btnSelector = ".btn-large-mktg.btn-mktg";
-    await page.waitForSelector(btnSelector, {
-      visible: true, timeout: 2000
-    });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Get started with Team")
+test("The page contains Sign in button", async () => {
+  const btnSelector = ".btn-large-mktg.btn-mktg";
+  await page.waitForSelector(btnSelector, {
+    visible: true, timeout: 2000
   });
+  const actual = await page.$eval(btnSelector, link => link.textContent);
+  expect(actual).toContain("Get started with Team")
+});
 });
